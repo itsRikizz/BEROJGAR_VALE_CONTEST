9710ef2d59d5cc0c2b0cb67cf7e5732543a68037
Sanmay Paine <97218362+itsRikizz@users.noreply.github.com>
2023-02-11 20:55:01 +0530

    test

--- Help James (Contest)/solution.java
+++ Help James (Contest)/solution.java
@@ -4,9 +4,8 @@ import java.util.*; // contains Collections framework
 // don't change the name of this class
 // you can add inner classes if needed
 
-
 class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         for (int i = 0; i < t; i++) {
